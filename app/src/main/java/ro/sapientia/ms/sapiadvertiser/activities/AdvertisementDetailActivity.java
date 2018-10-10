2bd50e49833cb3f237915b3b471760f73f922e76
Bernadett Fekete <kozma.e.imre@gmail.com>
2018-10-10 21:15:41 +0300

    Adding empty activities

---
+++ app/src/main/java/ro/sapientia/ms/sapiadvertiser/activities/AdvertisementDetailActivity.java
@@ -0,0 +1,17 @@
+package ro.sapientia.ms.sapiadvertiser.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// if you put your Activities files to another folder than the default one. You need to import the
+// com.example.yourproject.R (this is your project R file NOT Android.R file) to ALL activities using R.
+import ro.sapientia.ms.sapiadvertiser.R;
+
+
+public class AdvertisementDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertisement_detail);
+    }
+}
---
+++ app/src/main/java/ro/sapientia/ms/sapiadvertiser/activities/MyAdvertisementsActivity.java
@@ -0,0 +1,47 @@
+package ro.sapientia.ms.sapiadvertiser.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+// if you put your Activities files to another folder than the default one. You need to import the
+// com.example.yourproject.R (this is your project R file NOT Android.R file) to ALL activities using R.
+import ro.sapientia.ms.sapiadvertiser.R;
+
+public class MyAdvertisementsActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_advertisements);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
